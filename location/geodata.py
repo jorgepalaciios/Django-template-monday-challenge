6107b4f1523f9542d93bb01cc59939e956fffa92
jorgepalaciios <jorgepalaciios91@gmail.com>
2022-11-13 00:55:12 -0400

    Chore: Location script seeder added

--- location/geodata.py
+++ location/geodata.py
@@ -1,74 +1,37 @@
 import json
-#from .models import Country, Province, City
+from location.models import Country, Province, City
 
+#Getting data from json files
 with open('json_data/countries_data.json') as file:
     cdata = json.load(file)
-with open('json_data/cities_data.json') as file:
-    rdata = json.load(file)
-
-
-for country in cdata:
-    country_id = country['id'],
-    country_name = country['name'],
-    country_code = country['code'],
-
 
 with open('json_data/provinces_data.json') as file:
     pdata = json.load(file)
 
+with open('json_data/cities_data.json') as file:
+    rdata = json.load(file)
 
-for province in pdata:
-    province_id = province['id'],
-    province_name = province['name'],
-    c_code = province['country']
+
+#Iterating through data obtained from json files
+for country in cdata:
+    country_object = Country.objects.create(
+			name=country['name'],
+			code=country['code']
+          )
+
+    country_provinces=list(filter(lambda province: province['country']==country['code'], pdata))
     
-
-# p=list(filter(lambda c_code: c_code==country_code), pdata)
-# print(p)
-
-for city in rdata:
-    city_name = city['name'],
-    c_id = city['countryId'],
-    p_id = city['provinceId']
-
-
-
-
- 
-# list(filter(lambda p: country_code==country, province_data))
-
-
-# def generate_country():
-# 	for country in range(count):
-# 		country_name = get_countryname() # i think i should replace this with another logic 
-# 		country_code = generate_countrycode()
-
-# 		Country.objects.create(
-# 			name=country_name,
-# 			code=country_code
-#           ) 
-
-# city_data_json file 
-# {
-#   "id": 1,
-#   "name": "Agdam",
-#   "provinceId": 1,
-#   "countryId": 3
-# },
-
-# province_daja_json file
-
-#  {
-#     "id": 6,
-#     "country": "BY",
-#     "name": "Vitebskaya obl."
-#   },
-
-#im trying to figure out things up here, to catch them down
-#and then create the instances need it for django
-#to generate the data for the db
-
-# 
-# funcion filter*** create iteration inside to continue getting the data
-#to the other 
-# filter list of dictionaries (search)
+    for province in country_provinces:
+        province_object = Province.objects.create(
+            name=province['name'],
+            country=country_object
+        )
+        
+        province_cities=list(filter(lambda city: city['provinceId']==province['id'], rdata))
+        
+        for city in province_cities:
+            city_object = City.objects.create(
+                name=city['name'],
+                country=country_object,
+                province=province_object
+            )
--- location/models.py
+++ location/models.py
@@ -2,23 +2,32 @@ from django.db import models
 
 # Create your models here.
 class Country(models.Model):
-    name = models.CharField( max_length=50, unique=True)
+    name = models.CharField( max_length=200, unique=True)
     code = models.CharField( max_length=10, unique=True)
 
     class Meta:
         db_table = 'countries'
+    def __str__(self):
+        return self.name
+    
 
 class Province(models.Model):
-    name = models.CharField( max_length=50)
+    name = models.CharField( max_length=200)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'provinces'
+    
+    def __str__(self):
+        return self.name
 
 class City(models.Model):
-    name = models.CharField( max_length=50)
+    name = models.CharField( max_length=200)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     province = models.ForeignKey(Province, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'cities'
+
+    def __str__(self):
+        return self.name
