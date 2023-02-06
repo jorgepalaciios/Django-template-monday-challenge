2f4466d2a8dbaed1dbdbebcc0a00d243e27006aa
jorgepalaciios <jorgepalaciios91@gmail.com>
2023-02-06 18:01:05 -0400

    feat: adding chat and chat messages models

--- chat_messages/models.py
+++ chat_messages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from chats.models import Chat
 
-# Create your models here.
+class ChatMessage(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    username = models.CharField( max_length=120 )
+    body = models.TextField()
+
+    def __str__(self):
+        return f"{self.chat.room_name}-{self.username}"
+    
+
--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Chat(models.Model):
+    room_name = models.CharField( max_length=120)
+
+    def __str__(self):
+        return self.room_name
+
+    def get_messages(self):
+        return self.messages.all()
+    
